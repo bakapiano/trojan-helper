18bebb61915cd673598a4875b2eb12c29c7d09e7
bakapiano <bakapiano@outlook.com>
2023-05-06 11:55:26 +0800

    init

---
+++ index.js
@@ -0,0 +1,64 @@
+import express from "express";
+import fetch from "node-fetch";
+import fs from "fs";
+import schedule from "node-schedule"
+import subProcess from "child_process";
+
+const TROJAN_CONFIG_PATH = "./config.json"
+const TROJAN_PASSWORD = ""
+
+function readTrojanConfig() {
+    const trojanConfig = JSON.parse(fs.readFileSync(TROJAN_CONFIG_PATH).toString())
+    return {
+        port: trojanConfig.local_port,
+        host: trojanConfig.ssl.sni,
+    }
+}
+
+const app = express()
+app.get("/config.yaml", (req, res) => {
+    let config = fs.readFileSync("./config.yaml").toString()
+    const { host, port } = readTrojanConfig()
+    config = config
+        .replace("{trojan-server}", host)
+        // .replace("{trojan-server}", host)
+        .replace("{trojan-port}", port)
+        .replace("{trojan-password}", TROJAN_PASSWORD)
+        
+    res.set('Content-Type', 'application/yaml').status(200).send(config)
+})
+app.listen(8848)
+
+async function ping(ip, port) {
+    const res = await fetch(`https://www.jkapi.com/api/tcpck?ip=${ip}&port=${port}`)
+    return (await res.json()).status === "OPENNING"
+}
+
+async function getPublicIp() {
+    const res = await fetch('https://api64.ipify.org?format=json')
+    return (await res.json()).ip
+}
+
+const rule = new schedule.RecurrenceRule()
+rule.minute = []
+for (var min = 0; min < 60; min += 1) rule.minute.push(min)
+schedule.scheduleJob(rule, async () => {
+    const PING_RETRY_COUNT = 5
+    const ip = await getPublicIp()
+    const { port } = readTrojanConfig()
+    console.log(ip, port)
+    for (var i = 0; i < PING_RETRY_COUNT; ++i) {
+        if (ping(ip, port)) {
+            return
+        }
+    }
+    subProcess.exec('echo "" | trojan port', (err, stdout, stderr) => {
+        if (err) {
+            console.error(err)
+            process.exit(1)
+        } else {
+            console.log(`The stdout Buffer from shell: ${stdout.toString()}`)
+            console.log(`The stderr Buffer from shell: ${stderr.toString()}`)
+        }
+    })
+})
